ff2688b0bfd4d5142acb85414502dac1a1689166
Marcos Postemsky <marcos.postemsky@gmail.com>
2018-05-14 19:01:22 -0300

    Agrego señal de control lectura escritura y comienzo proceso (pensar si se agregar un reset)

--- src/bank_reg.vhd
+++ src/bank_reg.vhd
@@ -11,11 +11,12 @@ ENTITY bank_reg IS
     PORT(
       A_i : IN     std_logic_vector(n-1 downto 0);
       B_i : IN     std_logic_vector(n-1 downto 0);
-      C_i : IN     std_logic_vector(n-1 downto 0);
+      C_i : IN     std_logic_vector(n-1 downto 0);    
+      R_W_i: IN		std_logic;
       CLK_i : IN     std_logic;
       W_c_i : IN     std_logic_vector(n-1 downto 0);
       R_a_o : OUT    std_logic_vector(n-1 downto 0);
-      R_b_o : OUT    std_logic_vector(n-1 downto 0));
+      R_b_o : OUT    std_logic_vector(n-1 downto 0))
 END ENTITY bank_reg;
 
 ARCHITECTURE Behavioral OF bank_reg IS
@@ -25,10 +26,23 @@ TYPE ram_memory IS ARRAY ( 2**bit_dir_reg-1 downto 0 ) OF std_logic_vector(n_reg
 signal Memoria_ram: ram_memory;
 
 BEGIN
- 
+
+Registros : PROCESS (CLK_i, rst) IS
+  -- Put constant and variable declarations here.
+BEGIN
+  IF (rst = '1') THEN
+    -- Put reset assignments here.
+  ELSIF (rising_edge(CLK_i))  THEN
+    -- Put clocked assignments here. 
+    
+    
+    
+  END IF;
+END PROCESS Registros;
+
  
  
   -- put concurrent statements here.
 END ARCHITECTURE Behavioral; -- Of entity bank_reg
 
-
+ 
